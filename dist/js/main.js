d5cad8267bbafd9b444343dcd7c258a672f13a8b
T.H <Thomashendrikz@Gmail.com>
2020-07-08 22:36:09 +1200

    help

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,3 +4,19 @@ const navMenu = $(".nav-menu");
 toggleMenuBtn.click(() => {
   navMenu.slideToggle(550, "easeInOutBack");
 });
+
+$("document").ready(() => {
+  let dateFormat = "dd/mm/yy";
+
+  $(() => {
+    let form = $(".from").datepicker({
+      firstDay: 1,
+      minDate: 0,
+      changeMonth: true,
+      constrainInput: true,
+      showButtonPanel: true,
+      currentText: "Today",
+      closeText: "Close",
+    });
+  });
+});
