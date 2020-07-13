f653e079ff62707790a09686338d1492c3bef97e
T.H <Thomashendrikz@Gmail.com>
2020-07-13 17:07:36 +1200

    looking good

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,7 +12,8 @@ const submitBtn = $(".inputbox__bottom--btn");
 // INPUT FEILD ---------------------
 //**********************************
 let pickUpDate = {};
-let pickUpdate = {};
+let dropOffDate = {};
+let rentTimeRange;
 let pickUpLocation = $(".pick-up-location").selectmenu();
 let dropOffLocation = $(".drop-off-location").selectmenu();
 let partySize = $(".peopleCount").spinner({
@@ -21,19 +22,22 @@ let partySize = $(".peopleCount").spinner({
 });
 $(".peopleCount").spinner("value", 1);
 // CALENDAR -- LIGHT PICKER
-let picker = new Lightpick({
-  field: document.getElementById("datepicker-from"),
-  secondField: document.getElementById("datepicker-to"),
-  singleDate: false,
-  minDate: moment().startOf("month").add(0, "day"),
-  maxDays: 15,
-  selectForward: true,
-  onSelect: function (start, end) {
-    let str = "";
-    str += start ? start.format("Do MMMM YYYY") + " to " : "";
-    str += end ? end.format("Do MMMM YYYY") : "...";
-  },
-});
+// let picker = new Lightpick({
+//   field: document.getElementById("datepicker-from"),
+//   secondField: document.getElementById("datepicker-to"),
+//   repick: true,
+//   singleDate: false,
+//   startDate: moment().startOf("month").add(7, "day"),
+//   endDate: moment().add(14, "day"),
+//   minDate: moment().startOf("month").add(0, "day"),
+//   maxDays: 15,
+//   selectForward: true,
+//   onSelect: function (start, end) {
+//     let str = "";
+//     str += start ? start.format("Do MMMM YYYY") + " to " : "";
+//     str += end ? end.format("Do MMMM YYYY") : "...";
+//   },
+// });
 
 //**********************************
 // Events --------------------------
@@ -46,12 +50,14 @@ toggleMenuBtn.click(() => {
 submitBtn.click(() => {
   console.log("pick up from location " + pickUpLocation.val());
   console.log("Drop off at location " + dropOffLocation.val());
-  console.log("Party size is" + partySize.spinner("value"));
+  console.log("Party size is of " + partySize.spinner("value") + " people");
 
-  console.log(picker.getDate());
+  console.log(picker.getStartDate());
+  console.log(picker.getStartDate().date());
 
-  // pickUpDate = picker.getStartDate();
-  // dropOffDate = picker.getEndDate();
-  // console.log("pick up date " + pickUpDate);
-  // console.log("drop off date " + dropOffDate);
+  pickUpDate = picker.getStartDate();
+  dropOffDate = picker.getEndDate();
+  rentTimeRange = "Days of rent: " + (dropOffDate.diff(pickUpDate, "days") + 1);
+
+  console.log(rentTimeRange);
 });
